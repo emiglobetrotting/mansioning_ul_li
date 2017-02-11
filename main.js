7511c566426be25cfaaaf1a133bbc6b33663a707
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-02-11 19:49:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 require.config({
-  enforceDefine:true,
-  urlArgs:function(id, url){
-    var args = '';
-    if (url.indexOf('code.jquery.com/jquery-2.2.4.min') !== -1) {
-      args = 'integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="&crossorigin="anonymous"';
-    }
-    return (url.indexOf('?') === -1 ? '?' : '&') + args;
-  },
   paths:{
     jquery: [
-      'https://code.jquery.com/jquery-2.2.4.min',
-      'jquery.min'
+      'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min'
     ]
   }
 });
-require(['mansioning_ul_li']);
+require(['mansioning_ul_li'],function(mansioning_ul_li){
+});
