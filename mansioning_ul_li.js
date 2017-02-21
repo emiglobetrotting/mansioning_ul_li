8718c9c0915f6e25003cb14fa01235fc6eea0858
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-02-21 14:18:03 +0100

    Update mansioning_ul_li.js

--- mansioning_ul_li.js
+++ mansioning_ul_li.js
@@ -2,7 +2,7 @@
 define(['jquery','mansioning'],function($,mansioning){
 	$(function(){
 		var mansioning_ul_li = function(){
-			mansioning($("#mansionable-list"),"7px");
+			mansioning($("#mansionable-list"),"10px");
 		};
 		mansioning_ul_li();
 		$(window).resize(mansioning_ul_li);
