350ce02d4b2d7d0c8f1e330fb897244047e21474
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-02-21 13:27:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" Content="IE=edge">
+		<meta name="viewport" Content="width=device-width, initial-scale=1">
+
+		<link href="css/mansioning.css" rel="stylesheet">
+    
+ </head>
+ <body>
+    <div class="block auto-height overflow-hidden">
+        <ul id="mansionable-list" class="inline-ul">
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-150-px">
+                    ITEM HEIGHT 150PX
+                </div>
+            </li>
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-100-px">
+                    ITEM HEIGHT 100PX
+                </div>
+            </li>
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-300-px">
+                    ITEM HEIGHT 300PX
+                </div>
+            </li>
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-40-px">
+                    ITEM HEIGHT 40PX
+                </div>
+            </li>
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-140-px">
+                    ITEM HEIGHT 140PX
+                </div>
+            </li>
+            <li class="font-1 align-top margin-top-10-px">
+                <div class="width-250-px padding-left-right-10-px height-240-px">
+                    ITEM HEIGHT 240PX
+                </div>
+            </li>
+         </ul>
+     </div>
+ </body>
+</html>
