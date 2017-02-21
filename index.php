14ac3e5866fe1f99d369474a18f225cb16a1b48a
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-02-21 13:45:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,37 +7,37 @@
 
 		<link href="css/mansioning.css" rel="stylesheet">
     
- </head>
+       </head>
  <body>
     <div class="block auto-height overflow-hidden">
         <ul id="mansionable-list" class="inline-ul">
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-150-px">
+                <div class="width-250-px border padding-left-right-10-px height-150-px">
                     ITEM HEIGHT 150PX
                 </div>
             </li>
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-100-px">
+                <div class="width-250-px border padding-left-right-10-px height-100-px">
                     ITEM HEIGHT 100PX
                 </div>
             </li>
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-300-px">
+                <div class="width-250-px border padding-left-right-10-px height-300-px">
                     ITEM HEIGHT 300PX
                 </div>
             </li>
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-40-px">
+                <div class="width-250-px border padding-left-right-10-px height-40-px">
                     ITEM HEIGHT 40PX
                 </div>
             </li>
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-140-px">
+                <div class="width-250-px border padding-left-right-10-px height-140-px">
                     ITEM HEIGHT 140PX
                 </div>
             </li>
             <li class="font-1 align-top margin-top-10-px">
-                <div class="width-250-px padding-left-right-10-px height-240-px">
+                <div class="width-250-px border padding-left-right-10-px height-240-px">
                     ITEM HEIGHT 240PX
                 </div>
             </li>
