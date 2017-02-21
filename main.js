9f399ff12894ea7aff9ff36886dad1d6ecf2cb47
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-02-21 14:16:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 require.config({
   paths:{
     jquery: [
-      'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min'
+      'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min',
+	  'jquery.min'
     ]
   }
 });
