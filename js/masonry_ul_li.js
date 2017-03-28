b4e74cfbf5f9cf861fd672911c57c5978363f266
Tamunoemi Amachree <emiglobetrotting@yahoo.co.uk>
2017-03-28 15:58:20 +0100

    Update masonry_ul_li.js

--- js/masonry_ul_li.js
+++ js/masonry_ul_li.js
@@ -2,7 +2,7 @@
 define(['jquery','masonry'],function($,masonry){
 	$(function(){
 		var masonry_ul_li = function(){
-			masonry($("#masonry-list-id"),"10px");
+			masonry($("#mansionable-list"),"10px");
 		};
 		masonry_ul_li();
 		$(window).resize(masonry_ul_li);
